54e9318bf2c159a4d7f13df38f82ca8918926536
Boubekeur BENZAID <boubekeur_benzaid@marketpay.eu>
2021-07-05 21:21:11 +0200

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="navbar">
+        <ul class="menu">
+            <li class="menu-item">About</li>
+            <li class="menu-item">Skills</li>
+            <li class="menu-item">Experience</li>
+            <li class="menu-item">Portfolio</li>
+            <li class="menu-item">Contact</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: #333;
+}
+.menu {
+    display: flex;
+    list-style: none;
+}
+.menu-item {
+    padding: 10px;
+    color: #f1f1f1;
+}
+.menu-item:last-child {
+    margin-right: 2rem;
+}
+</style>
